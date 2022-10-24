8fa2cbd07467154420d2ef1aa2e90c3bf406d359
Magali Fort <75902373+mflynx@users.noreply.github.com>
2022-10-24 11:02:44 +0200

    add tries and pairs counts + refact compare

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,54 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { instruments } from "./../data/musicCards";
-import {getAllCards, CardType} from "./../utils/CardSetup"
-import shuffleCards from "./../utils/Shuffle"
+import { getAllCards, CardType } from "./../utils/CardSetup";
+import shuffleCards from "./../utils/Shuffle";
 import Card from "./Card";
 import styles from "./Game.module.scss";
 
-const allCards = getAllCards(instruments)
+const allCards = getAllCards(instruments);
 
 function Game() {
   const [cards, setCards] = useState<CardType[]>(shuffleCards(allCards));
   const [cardsPicked, setCardsPicked] = useState<CardType[]>([]);
+  const [triesCount, setTriesCount] = useState<number>(0);
+  const [pairsFound, setPairsFound] = useState<number>(0);
 
-  //   console.log(cards);
+  // console.log(cards);
 
   const resetGame = () => {
     setCardsPicked([]);
+    setTriesCount(0);
+    setPairsFound(0);
     setCards(shuffleCards(allCards));
   };
 
   const flipCard = (cardToFlip: CardType) => {
-    let updatedCards = cards.map((card) =>
+    const updatedCards = cards.map((card) =>
       card.id === cardToFlip.id ? { ...card, faceUp: !card.faceUp } : card
     );
     setCards(updatedCards);
   };
 
+  const compareCards = (secondPick: CardType) => {
+    const cardsMatch = cardsPicked[0].name === secondPick.name;
+    if (cardsMatch) {
+      setPairsFound(pairsFound + 1);
+      setCardsPicked([]);
+    } else {
+      setTimeout(() => {
+        cardsPicked.forEach((card) => flipCard(card));
+        setCardsPicked([]);
+      }, 1000);
+    }
+  };
+
   const handleClick = (selectedCard: CardType) => {
+    // allow only 2 cards to be revealed for each try
     if (cardsPicked.length === 2) return;
     flipCard(selectedCard);
     if (cardsPicked.length === 0) {
       setCardsPicked([selectedCard]);
     } else {
       setCardsPicked([...cardsPicked, selectedCard]);
-      let cardsMatch = cardsPicked[0].name === selectedCard.name;
-    //   console.log("cardsMatch :>> ", cardsMatch);
-
-      if (cardsMatch) {
-        //do smth if cardsMatch
-        //check winner
-        setCardsPicked([]);
-      } else {
-        setTimeout(() => {
-          cardsPicked.forEach((card) => flipCard(card));
-          setCardsPicked([]);
-        }, 1000);
-      }
+      setTriesCount(triesCount + 1);
+      compareCards(selectedCard);
     }
   };
 
-//   console.log("cardsPicked", cardsPicked);
+  useEffect(() => {
+    // check winner
+    if (pairsFound === cards.length / 2) {
+      //do smth when winner
+      console.log("You won !!! in " + triesCount + " tries");
+    }
+  }, [pairsFound, cards.length, triesCount]);
 
   return (
     <div>
